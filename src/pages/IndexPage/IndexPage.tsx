a32015c64a3e04f053e630117dcc92d4d6efe2ba
Vindiza Aziz <vindizaaziz@gmail.com>
2024-10-28 16:38:00 +0600

    fix burger

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,26 +1,17 @@
 import {FC} from 'react';
 import ProductCard from "@/components/ProductCard/ProductCard.tsx";
 import {List} from "@telegram-apps/telegram-ui";
+import burger from './burger-bar.png'
 
 export const IndexPage: FC = () => {
     return (
         <List>
             <div style={{display: "flex", justifyContent: "space-evenly", flexWrap: "wrap"}}>
-                <ProductCard title={"burger"}
-                             image={"https://png.pngtree.com/png-clipart/20220413/ourmid/pngtree-burger-icon-png-image_4541658.png"}
-                             price={9.39}/>
-                <ProductCard title={"burger"}
-                             image={"https://png.pngtree.com/png-clipart/20220413/ourmid/pngtree-burger-icon-png-image_4541658.png"}
-                             price={9.39}/>
-                <ProductCard title={"burger"}
-                             image={"https://png.pngtree.com/png-clipart/20220413/ourmid/pngtree-burger-icon-png-image_4541658.png"}
-                             price={9.39}/>
-                <ProductCard title={"burger"}
-                             image={"https://png.pngtree.com/png-clipart/20220413/ourmid/pngtree-burger-icon-png-image_4541658.png"}
-                             price={9.39}/>
-                <ProductCard title={"burger"}
-                             image={"https://png.pngtree.com/png-clipart/20220413/ourmid/pngtree-burger-icon-png-image_4541658.png"}
-                             price={9.39}/>
+                <ProductCard title={"burger"} image={burger} price={9.39}/>
+                <ProductCard title={"burger"} image={burger} price={9.39}/>
+                <ProductCard title={"burger"} image={burger} price={9.39}/>
+                <ProductCard title={"burger"} image={burger} price={9.39}/>
+                <ProductCard title={"burger"} image={burger} price={9.39}/>
             </div>
         </List>
     );
